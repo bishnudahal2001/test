096fff17ac2738b1970a78486e0beee26e3858e1
Bishnu Dahal <koalastoner101@gmail.com>
2021-11-14 17:48:41 -0500

    iniital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify-app';
+  constructor(private readonly _http: HttpClient) {
+    _http.get('https://2xmk4kt2r2.execute-api.us-east-1.amazonaws.com/dev/home').subscribe((message) => {
+    console.log(message);
+    this.title = message as string;
+    
+    })
+
+  }
 }
